949cd184d846b177b7b3673d78bcf75631a915d7
Reny Yohan Palencia <yohan_21_@hotmail.com>
2022-04-01 21:26:29 -0400

    “prueba”

---
+++ precache-manifest.8f392ab41b2b370131e71ccef8af60a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "989c246c8ce3c332ad86b9c51d87b203",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1004dadedd7b800af3bc",
+    "url": "/static/css/main.f75365bb.chunk.css"
+  },
+  {
+    "revision": "70e17c3dced10d991999",
+    "url": "/static/js/2.8b1fd144.chunk.js"
+  },
+  {
+    "revision": "4150a627f5138970747231fcef63f425",
+    "url": "/static/js/2.8b1fd144.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1004dadedd7b800af3bc",
+    "url": "/static/js/main.a5a3b4fa.chunk.js"
+  },
+  {
+    "revision": "b392ba862a5bbfe037c8",
+    "url": "/static/js/runtime-main.f24b64df.js"
+  }
+]);
